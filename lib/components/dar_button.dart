a20fd499531b4198bf05bf2de379227fb9f14692
mayaalqurshi <maya.alqurshi@gmail.com>
2023-03-03 13:14:17 +0300

    dar project

---
+++ lib/components/dar_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DarButton extends StatelessWidget {
+  const DarButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: null,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:dar/dar_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DarApp());
+}

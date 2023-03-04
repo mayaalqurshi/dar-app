07c03d6406c8495d4ca8bfe109ba4a7edb218bea
Rahafsaq <rahafalqahtani11@hotmail.com>
2023-03-04 21:22:59 +0300

    aboutUs and sectionPage added

---
+++ lib/pages/section_page.dart
@@ -0,0 +1,64 @@
+import 'package:dar/components/logo.dart';
+import 'package:dar/components/title1.dart';
+import 'package:flutter/material.dart';
+
+import '../components/section_card.dart';
+
+class SectionPage extends StatelessWidget {
+  const SectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const InkWell(
+          // onTap: () {
+          //   Navigator.pop(context);
+          // },
+          child: Icon(
+            Icons.arrow_back_ios_new_sharp,
+            color: Colors.white,
+          ),
+        ),
+        title: const Center(child: Logo()),
+        actions: const [
+          Center(
+            child: Padding(
+                padding: EdgeInsets.only(right: 12),
+                child: Icon(
+                  Icons.list,
+                  color: Colors.black,
+                )),
+          )
+        ],
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(right: 68),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [Title1('الأقسام')],
+                ),
+              ),
+              const SizedBox(height: 50),
+              const SectionCard(),
+              const SizedBox(height: 20),
+              const SectionCard(),
+              const SizedBox(height: 20),
+              const SectionCard(),
+              const SizedBox(height: 20),
+              const SectionCard(),
+              const SizedBox(height: 50),
+            ],
+          )
+        ],
+      ),
+      backgroundColor: const Color.fromARGB(255, 231, 228, 228),
+    );
+  }
+}
